361e67d778faedf88d117141451f6564de3ce3c9
EUNBIN0811 <seoeunbin0811@naver.com>
2025-05-21 14:54:04 +0900

    Delete ros2_term_project directory

--- ros2_term_project/src/cmd_publisher_node.cpp
+++
@@ -1,9 +0,0 @@
-#include "rclcpp/rclcpp.hpp"
-#include "cmd_publisher.h"
-
-int main(int argc, const char *argv[]) {
-    rclcpp::init(argc, argv);
-    rclcpp::spin(std::make_shared<CmdPublisher>());
-    rclcpp::shutdown();
-    return 0;
-}
